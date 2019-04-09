599c97e91d5107e277e24bd7b6913dabee898541
NoaJunod <noa.junod@googlemail.com>
2019-04-09 18:31:57 +0200

    client-sided data sending

--- src/main/java/servertest/ClientTest.java
+++ src/main/java/servertest/ClientTest.java
@@ -1,5 +1,6 @@
 package servertest;
 
+import game.GameData;
 import game.PlayerData;
 
 import java.io.*;
@@ -10,19 +11,21 @@ public class ClientTest {
     private int port;
     private String host;
     private PlayerData playerData;
+    private GameData gameData;
 
     public ClientTest(int port, String host, String name) {
         this.port = port;
         this.host = host;
         playerData = new PlayerData(name);
+        gameData = new GameData();
     }
 
     public void test() {
         try {
             Socket socket = new Socket(host, port);
-            String message = "Hello there, I'm " + playerData.getName();
             sendPlayerData(socket, playerData);
-            System.out.println(readMessage(socket));
+            readGameData(socket);
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,22 +35,18 @@ public class ClientTest {
         try {
             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
             outputStream.writeObject(playerData);
-            outputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public String readMessage(Socket socket) {
+    public void readGameData(Socket socket){
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            char[] buffer = new char[200];
-            int amoutChars = bufferedReader.read(buffer, 0, 200);
-            String message = new String(buffer, 0, amoutChars);
-            return message;
-        } catch (IOException e) {
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            gameData = (GameData) inputStream.readObject();
+            System.out.println(gameData.getAllCards().get(0).getName());
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
